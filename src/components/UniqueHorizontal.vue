0017f50045669017e95564e703bca99ff5d45fd5
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2025-03-22 14:16:23 +0100

    scroll horizontal

--- src/components/UniqueHorizontal.vue
+++ src/components/UniqueHorizontal.vue
@@ -1,29 +1,51 @@
 <script setup>
 import BaseButton from './CompButton.vue'
+
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {
+  gsap.to('.slider-container', {
+    x: '-75%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.slider-mask',
+      start: 'top top',
+      end: '+=2000' /* Ajuste la longueur du scroll */,
+      scrub: 1,
+      pin: true,
+    },
+  })
+})
 </script>
 
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <div class="row">
-      <div class="col-lg-6">
-        <h3>La Cité des Rêves: un univers en constante évolution</h3>
-      </div>
-      <div class="col-lg-6">
-        <p>
-          Plongez dans un espace collectif où chaque rêveur contribue à façonner un monde sans
-          limites.
-        </p>
-      </div>
-      <div class="col-lg-6">
-        <p>
-          La Neurosphère vous permet d'interagir avec d'autres utilisateurs, d'explorer ensemble et
-          de découvrir une réalité onirique partagée.
-        </p>
-      </div>
-      <div class="col-lg-6">
-        <p>
-          Chaque nuit devient une aventure, un lieu d'exploration et de rencontres inoubliables.
-        </p>
+      <div class="col-lg-12 section-parent">
+        <div class="section">
+          <h3>La Cité des Rêves: un univers en constante évolution</h3>
+        </div>
+        <div class="section">
+          <p>
+            Plongez dans un espace collectif où chaque rêveur contribue à façonner un monde sans
+            limites.
+          </p>
+        </div>
+        <div class="section">
+          <p>
+            La Neurosphère vous permet d'interagir avec d'autres utilisateurs, d'explorer ensemble
+            et de découvrir une réalité onirique partagée.
+          </p>
+        </div>
+        <div class="section">
+          <p>
+            Chaque nuit devient une aventure, un lieu d'exploration et de rencontres inoubliables.
+          </p>
+        </div>
       </div>
     </div>
     <div class="row">
@@ -31,7 +53,78 @@ import BaseButton from './CompButton.vue'
         <BaseButton compButton="En savoir plus" />
       </div>
     </div>
+  </div> -->
+  <div class="slider-mask">
+    <div class="slider-container">
+      <div class="slider-element">
+        <div class="slider-content">
+          <h3>La Cité des Rêves: un univers en constante évolution</h3>
+        </div>
+      </div>
+      <div class="slider-element">
+        <div class="slider-content">
+          <p>
+            Plongez dans un espace collectif où chaque rêveur contribue à façonner un monde sans
+            limites.
+          </p>
+        </div>
+      </div>
+      <div class="slider-element">
+        <div class="slider-content">
+          <p>
+            La Neurosphère vous permet d'interagir avec d'autres utilisateurs, d'explorer ensemble
+            et de découvrir une réalité onirique partagée.
+          </p>
+        </div>
+      </div>
+      <div class="slider-element">
+        <div class="slider-content">
+          <p>
+            Chaque nuit devient une aventure, un lieu d'exploration et de rencontres inoubliables.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slider-mask {
+  overflow: hidden;
+  height: 100vh;
+}
+
+.slider-container {
+  padding: 180px;
+  display: flex;
+  height: 100%;
+  width: 400vw;
+}
+
+.slider-element {
+  width: 100vw;
+  height: 100vh;
+  flex-shrink: 0;
+}
+
+.slider-content {
+  width: 60%; /* Définit la largeur du contenu */
+}
+
+@media (max-width: 576px) {
+  .slider-content {
+    width: 90%;
+  }
+}
+
+@media (min-width: 992px) {
+  .slider-content {
+    width: 40%;
+  }
+}
+
+/* width: 570px; */
+/* .section-parent {
+  display: flex;
+} */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { gsap } from 'gsap'
 
 import './assets/main.scss'
 
 createApp(App).mount('#app')
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
